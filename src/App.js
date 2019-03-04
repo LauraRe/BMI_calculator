432ddf01d31c41d7f05176d4618909e2c31a9a20
olivia <olivia@evolve.se>
2019-03-04 14:08:12 +0100

    add feature test file and create test for both Metric method and Imperial method

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ class App extends Component {
       <div className="App">
         <div>
           <label>Weight(kg)</label>
-          <input name="weight" value={this.state.weight} onClick={this.setState({ weight: e.target.value})}/>
+          <input name="weight" value={this.state.weight} onChange={(e) => this.setState({ weight: e.target.value})}/>
         </div>
         
         <div>
           <label>Height(cm)</label>
-          <input name="height" value={this.state.height} onClick={this.setState({height: e.target.value})} />
+          <input name="height" value={this.state.height} onChange={(e) => this.setState({height: e.target.value})} />
         </div>
 
         <DisplayResult
